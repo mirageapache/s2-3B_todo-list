bc6fc9e5de8e1316fbe58afde0ee4e97109b63ce
James <mirageapache@gmail.com>
2023-01-07 14:37:17 +0800

    Add delete todo function

--- app.js
+++ app.js
@@ -84,6 +84,13 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 // 伺服器監聽
 app.listen(port, () => {
